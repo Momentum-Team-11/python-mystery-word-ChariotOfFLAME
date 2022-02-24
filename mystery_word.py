7438b1fc6292145e923f81eac7d43d1b7874b891
Adam Lindgren <adam@adamlindgren.com>
2022-02-24 17:51:39 -0500

    mvp complete

--- mystery_word.py
+++ mystery_word.py
@@ -1,14 +1,16 @@
-from tokenize import Number
 import string
+from random import choice
+
 
 def play_game():
     # play = True
     # while play is True:
-        file = open("test-word.txt")  # Open the file
-        word = file.read().upper()  # read the file and make it a string that is uppercase
-        word  = word.replace("\n", "")  # erase the \n
+        file = open("words.txt")  # Open the file
+        words = file.read().upper().split()  # read the file and make it a string that is uppercase
+        word = choice(words)
+        word = word.replace("\n", "")  # erase the \n
         wordList = []  # make an empty list for the word
-        wordList[:0]=word  # take each letter and make them the items of the list
+        wordList[:0] = word  # take each letter and make them the items of the list
         guessList = []  # make a list for the guessed letters
         print(f"Your word is {len(word)} letters long.")  # tell the player how long the word is
         resultsList = str("_ " * len(word)).split()  # create a list of for the results with each item being "_ "
@@ -19,34 +21,34 @@ def play_game():
             results = ' '.join([str(item) for item in resultsList])  # create a text string to show the results to the player
             print(f"""
 {results}
-""")
-            print(f"You've guessed{guessedLetters}")
-            playerLetter = input(f"You have {guesses} incorrect guesses left. What letter do you guess?").upper()
+""")  # show the user the current results
+            print(f"You've guessed{guessedLetters}")  # show the user the letters they've guessed
+            playerLetter = input(f"You have {guesses} incorrect guesses left. What letter do you guess?").upper()  # uppercase the users input
             print("_____________________________________________")
 # ---------- HANDLE ERRORS --------------- #
-            if playerLetter.isdigit():
+            if playerLetter.isdigit():  # filter out numbers
                 print("Only letters are in this word!")
-                continue
-            if playerLetter in string.punctuation:
+                continue  # start the while loop over
+            if playerLetter in string.punctuation:  # filter out symbols
                 print("Only letters are in this word!")
-                continue
-            if playerLetter == " ":
+                continue  # start the while loop over
+            if playerLetter == " ":  # filter out spaces
                 print("Only letters are in this word!")
-                continue
-            if len(playerLetter) != 1:
+                continue  # start the while loop over
+            if len(playerLetter) != 1:  # filter out more than one letter
                 print("Just one letter please!")
-                continue
-            if playerLetter in guessList:
+                continue  # start the while loop over
+            if playerLetter in guessList:  # filter out previous guesses
                 print("WHOOPS! You guessed this letter already. Try a different one!")
-                continue
+                continue  # start the while loop over
 # ---------- END ERROR HANDLING ---------- #
             if playerLetter not in wordList:  # if they guess wrong...
                 guesses = guesses - 1  # decriment the incorrect guess counter
                 guessList.append(playerLetter)  # add the guessed letter to the LIST of guessed letters
                 guessedLetters += f" {playerLetter}, "  # add the guessed letter to the STRING of guessed letters
-                print(f"""
+                print("""
 OH NO! That wasn't right. Try again
-""")
+""")  # and start the while loop over
             else:  # if they guess right...
                 idx = 0  # create an index counter for the results list
                 for letter in wordList:
@@ -61,7 +63,7 @@ OH NO! That wasn't right. Try again
         if guesses == 0:  # if you run out of inccorect guesses...
             print(f"You ran out of guesses... the word was {word}. Better luck next time!")
         if resultsList == wordList:  # if the results list is the same as the word list...
-            results = ' '.join([str(item) for item in resultsList]) # create a text string to show the results to the player
+            results = ' '.join([str(item) for item in resultsList])  # create a text string to show the results to the player
             print(results)
             print(f"""CONGRADULATIONS! The word was {word}!
 You guessed{guessedLetters}, which means...
@@ -69,6 +71,7 @@ You got the word in {guessCounter} guesses!""")
         # playAgain = input("Would you like to play again?")
         # if playAgain.lower() == "no" or "n":
         #     play = False
+
 
 if __name__ == "__main__":
     play_game()
